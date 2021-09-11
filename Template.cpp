d3dc47498fec26fa5aec37130e90f4b5f4c6bfb0
wlaffey <willlaffey@gmail.com>
2021-09-10 21:49:29 -0700

    added files from windows machine

---
+++ Template.cpp
@@ -0,0 +1,46 @@
+//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+//* Program name    : .cpp                                          *   
+//*                                                                     * 
+//* Written by      : William Laffey                                    * 
+//*                                                                     * 
+//* Purpose         : 
+//*                                                                     * 
+//* Inputs          : 
+//*                                                                     * 
+//* Outputs         : 
+//*                                                                     * 
+//* Calls           : 
+//*                                                                     * 
+//* Structure       : BEGIN                                             * 
+//*                        
+//*                   STOP                                              * 
+//*                        End of program.                              *
+//*                                                                     * 
+//*---------------------------------------------------------------------* 
+//* Change Log:                                                         * 
+//*                            Revision                                 * 
+//*   Date        Changed  Rel Ver Mod Purpose                          * 
+//* 00/00/21      wlaffey  001.000.000 Initial release of program       * 
+//*                                                                     * 
+//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() 
+{
+    //Header
+    cout << endl << "William Laffey\t"
+         << "L1-1\t"
+         << "L1-1.exe" << endl;
+
+    
+
+    //Waits for input to close program
+    char q;
+    cout << "\nPress any key to Exit.";
+    cin.ignore(2, '\n');
+    cin.get(q);
+
+    return 0;
+}
