80c3cbf8e0c0d874945be53a382d63e1c8971748
wlaffey <willlaffey@gmail.com>
2021-09-19 13:50:27 -0700

    rectangleType.h from book

---
+++ Lab2/L2-1/rectangleType.h
@@ -0,0 +1,61 @@
+#pragma once
+using namespace std;
+
+class rectangleType {
+    public:
+        void setDimension(double l, double w);
+        double getLength() const;
+        double getWidth() const;
+        double area() const;
+        double perimeter() const;
+        void print() const;
+        rectangleType();
+        rectangleType(double l, double w);
+        rectangleType doubleDimensions();
+
+    private:
+        double length;
+        double width;
+};
+
+void rectangleType::setDimension(double l, double w) {
+    if (1 >= 0)
+        length = l;
+    else
+        length = 0;
+
+    if ( w>= 0)
+        width = w;
+    else
+        width = 0;
+}
+
+double rectangleType::getLength() const {
+    return length;
+}
+
+double rectangleType::getWidth() const {
+    return width;
+}
+
+double rectangleType::area() const  { 
+    return length * width;  
+} 
+
+double rectangleType::perimeter() const {
+    return 2 * (length + width);
+}
+
+void rectangleType::print() const {
+    cout << "Length = " << length
+         << "; Width = " << width;
+}
+
+rectangleType::rectangleType() {
+    length = 0;
+    width = 0;
+}
+
+rectangleType::rectangleType(double l, double w) {
+    setDimension(l, w);
+}
