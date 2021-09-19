ba217a5523760d947df7fe99008292dee4e7a2f3
wlaffey <willlaffey@gmail.com>
2021-09-19 13:07:07 -0700

    added final Lab1 project

---
+++ Finished/Lab 1/L1-1/L1-1.cpp
@@ -0,0 +1,118 @@
+/*
+    Program name    : die.cpp
+
+    Written by      : William Laffey
+
+    Purpose         : Records how many times the sum of a certain 
+                      number of dice are rolled.
+
+    Inputs          : Number of dice to be rolled
+                      Number of times dice will be rolled
+
+    Outputs         : Number of times that each sum of dice were rolled 
+                      with a bar graph for visual reference
+
+    Calls           : Calls to header file called "die.h"
+
+    Structure       : BEGIN                                             
+                           Straight line code no sub-processes
+                      STOP                                              
+                           End of program 
+                                                                        
+   ---------------------------------------------------------------------
+    Change Log:                                                         
+                               Revision                                 
+      Date        Changed  Rel Ver Mod Purpose                          
+    09/12/21      wlaffey  001.000.000 Initial release of program       
+                                                                        */
+//include statements
+#include "die.h"
+
+using namespace std;
+
+int main()
+{
+	//Header
+    cout << endl << "William Laffey\t"
+         << "L1-1\t"
+         << "L1-1.exe" << endl;
+
+    //declarations
+    int dice, sum;
+    int largestNum = 0;
+    long rolls;
+    int array[36] = {0};
+    die die;
+
+    //asks for number of dice
+    cout << "\nEnter the number of Dies to use (4, 5, or 6): ";
+    cin >> dice;
+    if (dice == 4 || dice == 5 || dice == 6)
+    {
+        cout << endl;
+    }
+    else
+    {
+        cout << "Please enter a valid number." << endl;
+        return 0;
+    }
+
+    //asks for number of rolls
+    cout << "\nEnter the number of times the " << dice << " are rolled (2500, 3000, or 5000): ";
+    cin >> rolls;
+    if (rolls == 2500 || rolls == 3000 || rolls == 5000)
+    {
+        cout << endl;
+    }
+    else
+    {
+        cout << "Please enter a valid number." << endl;
+        return 0;
+    }
+
+    //rolls dice (got help from Bailey Nichols)
+    for (int i = 0; i < rolls; i++)
+    {
+        for (int j = 0; j < dice; j++)
+        {
+            die.roll();
+            sum = sum + die.getNum();
+        }
+        array[sum] += 1;
+        sum = 0;
+    }
+
+    // determines largest number for bar graph
+    for (int y = 0; y < (dice * 6 + 1); y++) 
+    {
+        if (array[y] > largestNum)
+        {
+            largestNum = array[y];
+        }
+    }
+
+    cout << "For " << dice << " dice, rolled " << rolls << " times" << endl;
+    cout << "Count %:---   0    10   20   30   40   50   60   70   80   90   100" << endl;
+    cout << "Sum    count  |....|....|....|....|....|....|....|....|....|....|" << endl;
+
+    //displays sum and count
+    for (int k = 0; k < (dice * 6 + 1); k++)
+    {
+        cout << " "   << setw(2) << k;
+        cout << "  (" << setw(4) << array[k] << ")   ";
+        float asterikWorth = largestNum * .02;
+        for (float l = 0; l < (array[k] / asterikWorth); l++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    //Waits for input to close program
+    char q;
+    cout << "\nPress any key to Exit.";
+    cin.ignore(2, '\n');
+    cin.get(q);
+
+    return 0;
+}
---
+++ Finished/Lab 1/L1-1/die.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include <vector>
+class die
+{
+public:
+    die()
+    {
+        srand(time(0));
+        num = 1;
+    }
+    //Default constructor
+    //Sets the default number rolled by a die to 1
+
+    void roll();
+    //Function to roll a die.
+    //This function uses a random number generator to randomly
+    //generate a number between 1 and 6, and stores the number
+    //in the instance variable num.
+
+    int getNum() const;
+    //Function to return the number on the top face of the die.
+    //Returns the value of the instance variable num.
+
+private:
+    int num; //The value of this instance ( 1 through 6) variable num.
+};
+
+void die::roll()
+{
+        num = (rand() % 6 + 1);
+}
+
+int die::getNum() const
+{
+    return num;
+}
