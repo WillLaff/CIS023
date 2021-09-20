2d078a5e9de58f9c989f1b2b7c0326e10d54858b
wlaffey <willlaffey@gmail.com>
2021-09-19 18:13:05 -0700

    almost done with L2-1

--- Lab2/L2-1/L2-1.cpp
+++ Lab2/L2-1/L2-1.cpp
@@ -34,7 +34,64 @@ int main()
          << "L2-1\t"
          << "L2-1.exe" << endl;
 
-    
+    //declare objects 
+    rectangleType r1(10, 10);
+    rectangleType r2(10, 10);
+    rectangleType sum;
+
+    //test pre increment
+    ++r1;
+    cout << "\nNew length and width is " << r1.getLength() << " and " << r1.getWidth() << endl;
+    //test post increment
+    r1++;
+    cout << "New length and width is " << r1.getLength() << " and " << r1.getWidth() << endl;
+    //test pre decrement
+    --r1;
+    cout << "New length and width is " << r1.getLength() << " and " << r1.getWidth() << endl;
+    //test post decrement
+    r1--;
+    cout << "New length and width is " << r1.getLength() << " and " << r1.getWidth() << endl;
+
+    //tests + operator
+    sum = r1 + r2;
+    cout << "New rectangle dimensions are " << sum.getLength() << " and " << sum.getWidth() << endl;
+    //tests - operator
+    sum = r1 - r2;
+    cout << "New rectangle dimensions are " << sum.getLength() << " and " << sum.getWidth() << endl;
+    //tests * operator
+    sum = r1 * r2;
+    cout << "New rectangle dimensions are " << sum.getLength() << " and " << sum.getWidth() << endl;
+
+    //tests == operator
+    if (r1 == r2)
+        cout << "r1 is the same as r2" << endl;
+    else   
+        cout << "r1 is not the same as r2" << endl;
+    //tests != operator
+    if (r1 != r2)
+        cout << "r1 is not equal to r2" << endl;
+    else   
+        cout << "r1 is equal to r2" << endl;
+    //tests <= operator
+    if (r1 <= r2)
+        cout << "r1 is less than or equal to r2" << endl;
+    else   
+        cout << "r1 is greater than r2" << endl;
+    //tests < operator
+    if (r1 < r2)
+        cout << "r1 is less than r2" << endl;
+    else   
+        cout << "r1 is greater than r2" << endl;
+    //tests >= operator
+    if (r1 >= r2)
+        cout << "r1 is greater than or equal to r2" << endl;
+    else   
+        cout << "r1 is less than r2" << endl;
+    //tests > operator
+    if (r1 > r2)
+        cout << "r1 is greater than r2" << endl;
+    else   
+        cout << "r1 is less than r2" << endl;
 
     //Waits for input to close program
     char q;
--- Lab2/L2-1/rectangleType.h
+++ Lab2/L2-1/rectangleType.h
@@ -3,23 +3,51 @@ using namespace std;
 
 class rectangleType {
     public:
-        void setDimension(double l, double w);
         double getLength() const;
         double getWidth() const;
         double area() const;
         double perimeter() const;
-        void print() const;
+        void setDimension(double l, double w);
+
+        friend ostream& operator<<(ostream&, const rectangleType&);
+        friend istream& operator>>(istream&, rectangleType&);
+
+        rectangleType operator + (const rectangleType&) const;
+        rectangleType operator - (const rectangleType&) const;
+        rectangleType operator * (const rectangleType&) const;
+        rectangleType operator ++ ();   
+        rectangleType operator ++ (int);
+        rectangleType operator -- ();   
+        rectangleType operator -- (int);
+        bool operator == (const rectangleType&) const;
+        bool operator != (const rectangleType&) const;
+        bool operator <= (const rectangleType&) const;
+        bool operator < (const rectangleType&) const;
+        bool operator >= (const rectangleType&) const;
+        bool operator > (const rectangleType&) const;
+
         rectangleType();
         rectangleType(double l, double w);
-        rectangleType doubleDimensions();
 
     private:
         double length;
         double width;
 };
 
+double rectangleType::getLength() const {
+    return length;
+}
+double rectangleType::getWidth() const {
+    return width;
+}
+double rectangleType::area() const  { 
+    return length * width;  
+} 
+double rectangleType::perimeter() const {
+    return 2 * (length + width);
+}
 void rectangleType::setDimension(double l, double w) {
-    if (1 >= 0)
+    if (l >= 0)
         length = l;
     else
         length = 0;
@@ -30,32 +58,83 @@ void rectangleType::setDimension(double l, double w) {
         width = 0;
 }
 
-double rectangleType::getLength() const {
-    return length;
-}
-
-double rectangleType::getWidth() const {
-    return width;
-}
-
-double rectangleType::area() const  { 
-    return length * width;  
-} 
-
-double rectangleType::perimeter() const {
-    return 2 * (length + width);
-}
-
-void rectangleType::print() const {
-    cout << "Length = " << length
-         << "; Width = " << width;
-}
-
 rectangleType::rectangleType() {
     length = 0;
     width = 0;
 }
-
 rectangleType::rectangleType(double l, double w) {
     setDimension(l, w);
+}
+
+ostream& operator<<(ostream&, const rectangleType&) {
+
+}
+istream& operator>>(istream&, rectangleType&) {
+
+}
+
+rectangleType rectangleType::operator + (const rectangleType& rectangle) const {
+    rectangleType newRectangle;
+    newRectangle.length = length + rectangle.length;
+    newRectangle.width = width + rectangle.width;
+    return newRectangle;
+}
+rectangleType rectangleType::operator - (const rectangleType& rectangle) const {
+    rectangleType newRectangle;
+    newRectangle.length = length - rectangle.length;
+    newRectangle.width = width - rectangle.width;
+
+    if (newRectangle.length < 0)
+        newRectangle.length = 0;
+    if (newRectangle.width < 0)
+        newRectangle.width = 0;
+
+    return newRectangle;
+}
+rectangleType rectangleType::operator * (const rectangleType& rectangle) const {
+    rectangleType newRectangle;
+    newRectangle.length = length * rectangle.length;
+    newRectangle.width = width * rectangle.width;
+    return newRectangle;
+}
+rectangleType rectangleType::operator ++ () {
+    this->length++;
+    this->width++;
+    return(*this);
+}  
+rectangleType rectangleType::operator ++ (int u) {
+    rectangleType temp = *this;
+    length++;
+    width++;
+    return temp;
+}
+rectangleType rectangleType::operator -- () {
+    this->length--;
+    this->width--;
+    return(*this);
+}
+rectangleType rectangleType::operator -- (int u) {
+    rectangleType temp = *this;
+    length--;
+    width--;
+    return temp;
+}
+
+bool rectangleType::operator == (const rectangleType& rectangle) const {
+    return (this->area() == rectangle.area());
+}
+bool rectangleType::operator != (const rectangleType& rectangle) const {
+    return (this->area() != rectangle.area());
+}
+bool rectangleType::operator <= (const rectangleType& rectangle) const {
+    return (this->area() <= rectangle.area());
+}
+bool rectangleType::operator < (const rectangleType& rectangle) const {
+    return (this->area() < rectangle.area());
+}
+bool rectangleType::operator >= (const rectangleType& rectangle) const {
+    return (this->area() >= rectangle.area());
+}
+bool rectangleType::operator > (const rectangleType& rectangle) const {
+    return (this->area() > rectangle.area());
 }
